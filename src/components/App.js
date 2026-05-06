e2b53c6ea7e280f2ba045ea54aef77348bcaa864
Aakash457 <114485477+Aakash457@users.noreply.github.com>
2026-05-06 22:16:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,28 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-import '../styles/App.css'
+import '../styles/App.css';
 
-function App(){
-
-  const [markdown,setMarkdown] = useState("")
-  const [loading,setLoading] = useState(true)
-
-  const handleChange = (e) =>{
-    setMarkdown(e.target.value)
-  }
-  
-  const changeLoadingVar = () => {
-    setLoading(false)
-  }
+function App() {
+  const [markdown, setMarkdown] = useState("# Start typing markdown...");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(changeLoadingVar,3000)
-  },[]);
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    if(loading){
-      return (<div><h1>Loading...</h1></div>)
-    }
- 
-    return(
-      <div className="app">
-        <textarea onChange={handleChange}/>
-        <ReactMarkdown className="preview" children={markdown}/>
-      </div>
-    )
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className="app">
+      <textarea value={markdown} onChange={(e) => setMarkdown(e.target.value)} />
+      <ReactMarkdown className="preview">
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  );
 }
-
-// class App extends React.Component{
-
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       markdown: "",
-//       loading: true
-//     }
-//     this.handleChange = this.handleChange.bind(this);
-//     this.changeLoadingVar = this.changeLoadingVar.bind(this);
-//   }
-//   handleChange(e){
-//     this.setState({
-//       markdown: e.target.value
-//     })
-//   }
-//   changeLoadingVar(){
-//     this.setState({
-//       loading: false
-//     })
-//   }
-
-//   componentDidMount(){
-//     setTimeout(this.changeLoadingVar,3000)
-//   }
-//   render(){
-//     if(this.state.loading){
-//       return (<div><h1>Loading...</h1></div>)
-//     }
- 
-//     return(
-//       <div className="app">
-//         <textarea onChange={this.handleChange}/>
-//         <ReactMarkdown className="preview" children={this.state.markdown}/>
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
