0a459723446c06c3591b7fc4fa1c0e4fdb4e344f
Aakash457 <114485477+Aakash457@users.noreply.github.com>
2026-05-06 22:26:14 +0530

    Refactor MarkdownEditor to use markdown directly

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactMarkdown from "react-markdown";
 
 function MarkdownEditor({ markdown, setMarkdown }) {
-  const [preview, setPreview] = useState("");
-
-  useEffect(() => {
-    setPreview(markdown);
-  }, [markdown]);
-
   return (
     <div className="editor-container">
       <textarea
         className="textarea"
         value={markdown}
         onChange={(e) => setMarkdown(e.target.value)}
+        placeholder="Write Markdown here..."
       />
 
       <div className="preview">
-        <ReactMarkdown>{preview}</ReactMarkdown>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
       </div>
     </div>
   );
